31d92feed46bbba5df97db267a6b1a699e222564
Main <main@Mains-MacBook-Air.local>
2024-09-11 19:38:15 +0700

    first

---
+++ app/data/card.tsx
@@ -0,0 +1,42 @@
+const cardData = [
+  {
+    cardTitle: "Rainbow Eucalyptus",
+    cardDescription: "Nature's living artwork.",
+    cardContent: "Its bark sheds in patches, revealing vibrant colors beneath.",
+    cardFooter: "Found in the Philippines and Indonesia.",
+  },
+  {
+    cardTitle: "The Blobfish",
+    cardDescription: "The world's ugliest animal?",
+    cardContent: "Its gelatinous body adapts to extreme deep-sea pressures.",
+    cardFooter: "Sadly, threatened by deep-sea fishing.",
+  },
+  {
+    cardTitle: "The Immortal Jellyfish",
+    cardDescription: "Defying the aging process.",
+    cardContent:
+      "It can revert to its juvenile state, potentially living forever.",
+    cardFooter: "A mystery of the ocean's depths.",
+  },
+  {
+    cardTitle: "The Mantis Shrimp",
+    cardDescription: "The fastest punch in the animal kingdom.",
+    cardContent: "Its claws can strike with the force of a .22 caliber bullet.",
+    cardFooter: "Also known for its incredible eyesight.",
+  },
+  {
+    cardTitle: "The Axolotl",
+    cardDescription: "The smiling salamander.",
+    cardContent:
+      "It can regenerate lost limbs and even parts of its brain and heart.",
+    cardFooter: "Critically endangered in its native Mexico.",
+  },
+  {
+    cardTitle: "The Dumbo Octopus",
+    cardDescription: "Named after Disney's flying elephant.",
+    cardContent: "It flaps its ear-like fins to swim in the deep sea.",
+    cardFooter: "One of the deepest-dwelling octopuses.",
+  },
+];
+
+export default cardData;
